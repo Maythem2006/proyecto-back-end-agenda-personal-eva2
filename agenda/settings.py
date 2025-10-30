d3acca9897424bceefe7ca144321d65915419e08
Maythem11O6 <wolf2.0game@gmail.com>
2025-10-30 16:43:22 -0300

    dominio agregado
    dominio agregado a settings

--- agenda/settings.py
+++ agenda/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = 'django-insecure-(kkh47*e)oe-k)pwwnn1mtvnvz@bo*%qme3zxp)&+8s^u*4@6r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.onrender.com'
+]
 
 
 # Application definition
